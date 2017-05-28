44e7a78f24f908fdc7d3cc00e17eaa1ba7605ca5
E6430 <trantuannguen2306@gmail.com>
2017-05-27 20:39:03 -0700

    create class

---
+++ src/main/java/test01/ClassTest01.java
@@ -0,0 +1,10 @@
+package test01;
+
+/**
+ * Created by E6430 on 05/27/17.
+ */
+public class ClassTest01 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
