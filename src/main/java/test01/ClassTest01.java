f380e75011b59ea628ada7d75d92f773c31daf0e
E6430 <trantuannguen2306@gmail.com>
2017-05-27 20:41:40 -0700

    fix class test

--- src/main/java/test01/ClassTest01.java
+++ src/main/java/test01/ClassTest01.java
@@ -5,6 +5,6 @@ package test01;
  */
 public class ClassTest01 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("commit 1");
     }
 }
